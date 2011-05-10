a3c7a6d9111af88be8ca2c96e58f80d0db1ed91c
Max Nylin <max@articstudios.se>
2011-05-10 15:39:54 +0200

    - JavaScript now makes use of Drupal.behaviours. - Moved the "create new term" to the bottom of the select list as a "last resort" option. - Added sin documentation and fixed some typos.

--- term_delete_replace.js
+++ term_delete_replace.js
@@ -1,36 +1,39 @@
-(function ($) {
-$(document).ready(function() {
+Drupal.behaviors.term_detelte_replace = {
+  attach: function(context, settings) {
 
-  function setFormState() {
-    if (!$('#edit-replacement-term-replace').is(':checked')) {
-      setElementEnabledState($('#edit-replacement-term'), false);
-      setElementEnabledState($('#edit-replacement-term-new'), false);
-    } else if ($('#edit-replacement-term').val() != 'create_replace_new_8563') {
-      setElementEnabledState($('#edit-replacement-term'), true);
-      setElementEnabledState($('#edit-replacement-term-new'), false);
-    } else {
-      setElementEnabledState($('#edit-replacement-term'), true);
-      setElementEnabledState($('#edit-replacement-term-new'), true); 
-    }
+    (function ($) {
+
+      function setFormState() {
+        if (!$('#edit-replacement-term-replace').is(':checked')) {
+          setElementEnabledState($('#edit-replacement-term'), false);
+          setElementEnabledState($('#edit-replacement-term-new'), false);
+        } else if ($('#edit-replacement-term').val() != '0') {
+          setElementEnabledState($('#edit-replacement-term'), true);
+          setElementEnabledState($('#edit-replacement-term-new'), false);
+        } else {
+          setElementEnabledState($('#edit-replacement-term'), true);
+          setElementEnabledState($('#edit-replacement-term-new'), true);
+        }
+      }
+
+      function setElementEnabledState($element, enabled) {
+        if (enabled) {
+          $element.removeAttr('disabled');
+          $element.parents('div.form-item').removeClass('disabled');
+        } else {
+          $element.attr('disabled', true);
+          $element.parents('div.form-item').addClass('disabled');
+        }
+      }
+
+      setFormState();
+      $('#edit-replacement-term-replace').bind('click.replacement-term-replace', setFormState);
+      $('#edit-replacement-term').bind('change.replacement-term', setFormState);
+
+      var $createOption = $('#edit-replacement-term option:last');
+      $createOption.addClass('new-term');
+
+    })(jQuery);
+
   }
-  
-  function setElementEnabledState($element, enabled) {
-    if (enabled) {
-      $element.removeAttr('disabled');
-      $element.parents('div.form-item').removeClass('disabled');
-    } else {
-      $element.attr('disabled', true);
-      $element.parents('div.form-item').addClass('disabled');
-    }
-  }
-  
-  setFormState();
-  $('#edit-replacement-term-replace').bind('click.replacement-term-replace', setFormState);
-  $('#edit-replacement-term').bind('change.replacement-term', setFormState);
-	
-	var $firstOption = $('#edit-replacement-term option:first');
-	$firstOption.addClass('new-term');
-	$firstOption.text($firstOption.text().replace('<--- ', '').replace(' --->', ''));
-	
-});
-})(jQuery);
+};

c8c494ecd0589dcf7f4eca52cd67c036a26838d3
Oleksandr Trotsenko <oleksandr.trotsenko@gmail.com>
2013-02-17 15:25:56 +0200

    Fixed documenation to the module and improved comments to it.

--- term_merge.api.php
+++ term_merge.api.php
@@ -9,9 +9,11 @@
  * Notify other modules when merging of 2 taxonomy terms occur.
  *
  * @param object $term_trunk
- *   Fully loaded taxonomy term object of the term trunk
+ *   Fully loaded taxonomy term object of the term trunk, term into which
+ *   merging occurs, aka 'destination'
  * @param object $term_branch
- *   Fully loaded taxonomy term object of the term branch
+ *   Fully loaded taxonomy term object of the term branch, term that is being
+ *   merged, aka 'source'
  * @param array $context
  *   Array $context as it is passed to term_merge_action() - you can get a
  *   little more info about context of merging from this array
@@ -20,8 +22,8 @@
 function hook_term_merge($term_trunk, $term_branch, $context) {
   // Here we might want to run a query like:
   // UPDATE {my_table}
-  //   SET term_tid = $term_branch->tid
-  //   WHERE term_tid = $term_trunk->tid
+  //   SET term_tid = $term_trunk->tid
+  //   WHERE term_tid = $term_branch->tid
   // Or do anything else that you own logic requires when merging of 2 terms
   // happens.
 }
